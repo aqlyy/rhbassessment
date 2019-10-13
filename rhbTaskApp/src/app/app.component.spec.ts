8e36e74760bad02f800728285e63767a4df42feb
Aqly <aqly@cyber-village.net>
2019-10-13 22:01:54 +0800

    Remove unused element and add new unit test

--- rhbTaskApp/src/app/app.component.spec.ts
+++ rhbTaskApp/src/app/app.component.spec.ts
@@ -1,17 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './view/landing/landing.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 describe('AppComponent', () => {
+  let location, router;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          { path: 'landing', component: LandingComponent }
+        ])
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LandingComponent
       ],
     }).compileComponents();
   }));
+
+  beforeEach(inject([Router, Location], (_router: Router, _location: Location) => {
+    location = _location;
+    router = _router;
+  }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -21,5 +35,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
+  }));
+  it(`should go to landing page url`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    router.navigate(['/landing']).then(() => {
+      expect(location.path()).toBe('/landing');
+    });
   }));
 });
